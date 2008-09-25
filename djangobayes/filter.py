80b0b4fbc3cd045952b90a8329696c25fc082cb6
waylan <waylan@localhost>
2008-09-25 22:56:49 +0000

    Added 'build_msg' helper to djangobayes.filter module. Use this to pass a data dict into filter.

--- djangobayes/filter.py
+++ djangobayes/filter.py
@@ -1,6 +1,14 @@
 from djangobayes.models import Bayes
 from djangobayes.util import DjangoClassifier
 from spambayes.hammie import Hammie
+from email import message_from_string
 
 db = DjangoClassifier(Bayes)
 filter = Hammie(db)
+
+build_msg(body, **kwargs):
+    """ Return a e-mail type message from a dict to pass into filter. """
+    msg = message_from_string(body)
+    for k, v in kwargs.items():
+        msg[k] = v
+    return m
