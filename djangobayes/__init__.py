185e9ceba293602ef9ebac04dbeb8c8fc6f8898d
waylan <waylan@localhost>
2008-09-25 22:56:35 +0000

    Renamed the 'sbayes' app to 'djangobayes'.

--- djangobayes/__init__.py
+++ djangobayes/__init__.py
@@ -1,5 +1,5 @@
 try:
-    from sbayes.models import Bayes
+    from djangobayes.models import Bayes
 except:
     # FIXME: this doesn't catch the error on syncdb. I had to comment out 
     # all lines for syncdb to work.
@@ -7,7 +7,7 @@ except:
     # DB not yet synced??
     pass
 else:
-    from sbayes.util import DjangoClassifier
+    from djangobayes.util import DjangoClassifier
     from spambayes.hammie import Hammie
 
     db = DjangoClassifier(Bayes)
