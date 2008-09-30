f60e321a6716b427010a548d72baabea1930a3a3
waylan <waylan@localhost>
2008-09-30 00:12:56 +0000

    Added training. Also renamed commentbayes/signals.py to commentbayes/listeners.py as its more appropriate.

--- commentbayes/listeners.py
+++ commentbayes/listeners.py
@@ -1,4 +1,5 @@
 from djangobayes.filter import filter, build_msg
+from django.contrib.comments.models import CommentFlag
 
 try:
     from django.conf.settings import HAM_LEVEL
@@ -10,6 +11,7 @@ try:
 except ImportError:
     SPAM_LEVEL = 0.8
 
+
 def get_mark(score):
     """ Return mark based on score. """
     if score < HAM_LEVEL:
@@ -18,17 +20,23 @@ def get_mark(score):
         return "spam"
     return "unsure"
 
-def on_comment_was_posted(sender, comment, request, **kwargs):
-    """ Listed for comment signal and filter comment. """
-    msg = build_msg(comment.comment,
+
+def make_msg(comment, request):
+    """ Return a message from a comment & request. """
+
+    return build_msg(comment.comment,
             author_ip = request.META.get('REMOTE_ADDR', ''),
             author_agent = request.META.get('HTTP_USER_AGENT', ''),
             referer = request.META.get('HTTP_REFERER', ''),
             author = comment.user_name
-    )
+        )
 
+
+def on_comment_was_posted(sender, comment, request, **kwargs):
+    """ Filter comment. """
+    
     try:
-        score = filter.score(msg)
+        score = filter.score(make_msg(comment, request))
     except:
         score = None
         mark = 'unsure'
@@ -46,3 +54,17 @@ def on_comment_was_posted(sender, comment, request, **kwargs):
         comment.is_removed = True
     comment.save()
 
+
+def on_comment_was_flagged(sender, comment, flag, created, request, **kwargs):
+    """ Train with comment. """
+
+    # Only train once
+    if created:
+        if flag.flag == CommentFlag.MODERATOR_APPROVAL:
+            # Train ham
+            filter.train(make_msg(comment, request), False)
+            filter.store()
+        elif flag.flag == CommentFlag.MODERATOR_DELETION:
+            # Train spam
+            filter.train(make_msg(comment, request), True)
+            filter.store()
--- commentbayes/models.py
+++ commentbayes/models.py
@@ -1,8 +1,11 @@
-from django.contrib.comments.signals import comment_was_posted
-from commentbayes.signals import on_comment_was_posted
+from django.contrib.comments.signals import comment_was_posted, \
+                                            comment_was_flagged
+from commentbayes.listeners import on_comment_was_posted, \
+                                   on_comment_was_flagged
 
 # Connect to signals
 comment_was_posted.connect(on_comment_was_posted)
+comment_was_flagged.connect(on_comment_was_flagged)
 
 # No models here.
 
